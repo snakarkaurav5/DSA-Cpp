15bf5097701a1220f58fe241f6a24575d25ef70b
sanskar kaurav <sanskarkourav5@gmail.com>
2026-05-26 09:51:44 +0530

    Add files via upload

---
+++ 2darray1.cpp
@@ -0,0 +1,9 @@
+//metrix 2d arry
+#include<iostream>
+using namespace std;
+int main(){
+    int students[3][3]={{100,100,100},
+                         {74,74,75},              
+                          {23,34,67}};
+         cout<<students[1][1]<<endl;
+         return 0; }            
