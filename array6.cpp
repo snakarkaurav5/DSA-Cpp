acc4614186824f9a7ba23f5cf8440222ced9cb09
sanskar kaurav <sanskarkourav5@gmail.com>
2026-07-19 07:11:17 +0530

    Add files via upload

---
+++ array6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+binarysearch(int *arr,int n,int key ){
+    int st =0,end=n-1;
+    while(st<=end){
+       int  mid=st+end/2;
+       if(arr[mid]==key){//key found
+        return mid;
+    }else if(arr[mid]<key){//2nd half
+        st=mid+1;
+
+    }else{//1sthalf
+        end =mid-1;
+
+    }
+         
+    }
+    return -1;
+}
+int main(){
+   int  arr[]={1,2,3,4,5,6,7,8,12,13,14,15,16};
+   int n=sizeof(arr)/sizeof(int);
+  cout<<binarysearch(arr,n,12)<<endl;
+   return 0;
+}
