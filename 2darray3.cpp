256187177c836834148665dd3a9b26b6948686f7
sanskar kaurav <sanskarkourav5@gmail.com>
2026-09-16 07:55:53 +0530

    Add files via upload

---
+++ 2darray3.cpp
@@ -0,0 +1,47 @@
+//spiral matrix
+#include<iostream>
+using namespace std;
+void spiralmatrix(int mat[4][4],int n,int m){
+    int srow, int scol;
+    int erow=n-1, int ecol=m-1;
+    while(srow<=erow && scol<=ecol){
+    //top
+    for(int j=scol;j<=ecol;j++){
+        cout<<mat[srow][j]<<",";
+    }
+
+
+    //right
+    for(int i=srow+1;i<=erow;i++){
+        cout<<mat[i][ecol]<<",";
+    }
+
+
+    //bottom
+    for(int j=ecol-1;j<=scol;j++){
+        cout<<mat[j][erow]<<",";
+    }
+
+
+
+    //left
+    for(int i=erow-1;i<=srow+1;i++){
+        cout<<mat[i][scol]<<",";
+    }
+    srow++;scol++;
+    erow--;ecol--;
+
+
+    
+cout<<endl;
+}
+int main(){
+    int matrix[4][4]={{1,2,3,4,}
+                         {5,,6,7,8,}
+                        {9,10,11,12,}
+                        { 13,14,15,16}};
+         spiralmatrix(mtrix,4,4);
+         return 0;              
+}
+ 
+}                   
