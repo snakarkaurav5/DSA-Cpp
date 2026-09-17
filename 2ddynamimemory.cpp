c4350374fb6a8d1e8aa2df0351f48c458ab85822
sanskar kaurav <sanskarkourav5@gmail.com>
2026-09-17 07:36:19 +0530

    Add files via upload

---
+++ 2ddynamimemory.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows; int cols;
+    cout<<"enter rows:";
+    cin>>rows;
+    cout<<"enter cols:";
+    cin>>cols;
+    int* *matrix=new int*[rows];
+    for(int i=0;i<rows;i++){
+        matrix[i]=new int[cols];
+    }
+
+int x=1;
+for(int i=0;i<rows;i++){
+    for(int j=0;j<cols;j++){
+        matrix[i][j]=x++;
+        cout<<matrix[i][j]<<",";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
